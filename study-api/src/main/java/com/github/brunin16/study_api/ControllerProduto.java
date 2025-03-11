bd192786e92f1b4b5e696f856494893316c3aa35
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-10 21:25:40 -0300

    feat:adding class

--- study-api/src/main/java/com/github/brunin16/study_api/ControllerProduto.java
+++ study-api/src/main/java/com/github/brunin16/study_api/ControllerProduto.java
@@ -1,5 +1,8 @@
 package com.github.brunin16.study_api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,13 +12,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduto {
 
-    @PostMapping
-    public ResponseEntity<String> create() {
-        return ResponseEntity.status(HttpStatus.CREATED).body("produto Criado");
+    @PostMapping()
+    public ResponseEntity<Produto> create(@RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(produto);
     }
 
     @PutMapping
@@ -24,8 +28,11 @@ public class ControllerProduto {
     }
 
     @GetMapping
-    public ResponseEntity<String> find() {
-        return ResponseEntity.ok().body("'content':{\n    'name':maçã,\n    'name':banana\n}");
+    public ResponseEntity<List<Produto>> find() {
+        List<Produto> produtos = new ArrayList<Produto>();
+        produtos.add(new Produto(1l, "maca"));
+        produtos.add(new Produto(2l, "banana"));
+        return ResponseEntity.ok().body(produtos);
     }
 
     @DeleteMapping
---
+++ study-api/src/main/java/com/github/brunin16/study_api/Produto.java
@@ -0,0 +1,13 @@
+package com.github.brunin16.study_api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Produto {
+    private Long id;
+    private String name;
+}
