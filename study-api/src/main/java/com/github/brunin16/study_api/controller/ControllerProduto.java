99b06abda2ec97d27fc27919b19b0bbac694ef26
brunin16 <bruedu16@gmail.com>
2025-03-24 21:05:42 -0300

    adding: get product by id

--- study-api/src/main/java/com/github/brunin16/study_api/controller/ControllerProduto.java
+++ study-api/src/main/java/com/github/brunin16/study_api/controller/ControllerProduto.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,8 +35,13 @@ public class ControllerProduto {
     }
 
     @GetMapping
-    public ResponseEntity<List<Produto>> find() {
+    public ResponseEntity<List<Produto>> getAll() {
         return ResponseEntity.ok().body(ps.getAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Produto> get(@PathVariable Long id) {
+        return ResponseEntity.ok().body(ps.get(id));
     }
 
     @DeleteMapping
